5698e704829ce61907a153aeb49d8d7752c2771c
anshuthopsee <thopseeanshu@gmail.com>
2023-08-30 07:07:31 +0100

    uninstalled unused dependency

--- src/hooks/usePathBasedPresetName.js
+++ src/hooks/usePathBasedPresetName.js
@@ -1,5 +1,4 @@
 import { useState, useEffect, useRef } from "react";
-import useUpdateEffect from "./useUpdateEffect";
 import { PRESETS } from "../const/presets";
 import { splitWords, joinWords, getHash } from "./utils";
 
--- src/main.jsx
+++ src/main.jsx
@@ -11,4 +11,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </DarkModeContextProvider>
   </React.StrictMode>
   ,
-)
+);
